090a0bc8b6bfe53aae6c28d44d7a862f043849fa
Joel Backschat <joel@cafecomjava.com.br>
2015-06-23 00:11:26 -0300

    atualização

---
+++ src/main/java/br/com/cafecomjava/springsecurity/SpringHello.java
@@ -0,0 +1,33 @@
+package br.com.cafecomjava.springsecurity;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SpringHello extends HttpServlet {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+		
+		resp.getWriter().println("foi");
+		super.doGet(req, resp);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+	}
+	
+}
